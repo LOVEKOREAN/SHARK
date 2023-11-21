a435a8f6118eef2397c8f8fc276e3cc5f9f4e458
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-11-21 16:54:23 +0900

    1

---
+++ board.h
@@ -0,0 +1,13 @@
+//프로토타이핑해보기
+#define BOARDSTATUS_OK   1
+#define BOARDSTATUS_NOK  0
+ 
+int board_initBoard(void);
+int board_printBoardStatus(void);
+
+int board_getBoardCoin(int pos); //괄호 안에가 입력이 있는지?
+int board_getSharkPosition(void);
+int board_stepShark(void);
+int board_getBoardStatus(int pos);
+
+ 
